6b433c76007105ed13efd2640c5bf71c09d5f408
DenisSavytskiy <denys.savytskyi.ri.2024@lpnu.ua>
2024-10-17 02:43:45 +0300

    first commit

---
+++ AP_Lab№6.4.іт/AP_Lab№6.4.іт.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+
+void generateArray(int* a, const int size) {
+    for (int i = 0; i < size; i++) {
+        a[i] = rand() % 46 - 10;
+    }
+}
+
+void printArray(const int* a, const int size) {
+    for (int i = 0; i < size; i++) {
+        std::cout << std::setw(5) << a[i];
+    }
+    std::cout << std::endl;
+}
+
+int findMaxIndex(const int* a, const int size) {
+    int maxIndex = 0;
+    for (int i = 1; i < size; i++) {
+        if (a[i] > a[maxIndex]) {
+            maxIndex = i;
+        }
+    }
+    return maxIndex;
+}
+
+int productBetweenZeros(const int* a, const int size) {
+    int firstZero = -1, secondZero = -1;
+    int product = 1;
+    bool foundFirst = false;
+
+    for (int i = 0; i < size; i++) {
+        if (a[i] == 0) {
+            if (!foundFirst) {
+                foundFirst = true;
+                firstZero = i;
+            }
+            else {
+                secondZero = i;
+                break;
+            }
+        }
+        else if (foundFirst) {
+            product *= a[i];
+        }
+    }
+    return (secondZero != -1) ? product : 0;
+}
+
+void printTransformedArray(const int* a, const int size) {
+    for (int i = 0; i < size; i += 2) {
+        std::cout << std::setw(5) << a[i];
+    }
+    for (int i = 1; i < size; i += 2) {
+        std::cout << std::setw(5) << a[i];
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    srand((unsigned)time(NULL));
+
+    int size;
+    std::cout << "size = ";
+    std::cin >> size;
+
+    int* arr = new int[size];
+
+    generateArray(arr, size);
+
+    printArray(arr, size);
+
+    int maxIndex = findMaxIndex(arr, size);
+    std::cout << "Max element index: " << maxIndex << std::endl;
+
+    int product = productBetweenZeros(arr, size);
+    std::cout << "Product of elements between 0 and 0: " << product << std::endl;
+
+    printTransformedArray(arr, size);
+
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,37 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include <cstdlib>
+#include <ctime>
+#include "../AP_Lab№6.4.іт/AP_Lab№6.4.іт.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+    TEST_CLASS(UnitTest1)
+    {
+    public:
+        TEST_METHOD(TestFindMaxIndex)
+        {
+            int arr[] = { 1, 5, 3, 9, 2 };
+            int result = findMaxIndex(arr, 5);
+            Assert::AreEqual(3, result);
+        }
+
+        TEST_METHOD(TestProductBetweenZeros)
+        {
+            int arr[] = { 1, 0, 2, 3, 0, 4 };
+            int result = productBetweenZeros(arr, 6);
+            Assert::AreEqual(6, result);
+        }
+        TEST_METHOD(TestGenerateArray)
+        {
+            srand((unsigned)time(NULL));
+            int arr[10];
+            generateArray(arr, 10);
+            for (int i = 0; i < 10; i++) {
+                Assert::IsTrue(arr[i] >= -10 && arr[i] <= 35);
+            }
+        }
+    };
+}
